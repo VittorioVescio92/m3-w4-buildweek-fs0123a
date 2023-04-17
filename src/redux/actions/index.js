aacaf7c468f1f0fee7551ac524fd1788f487cf05
phyxius23 <30ntin@gmail.com>
2023-04-17 17:52:45 +0200

    fetch

---
+++ src/redux/actions/index.js
@@ -0,0 +1,28 @@
+// endpoint
+const endpointUserProfile = "https://striveschool-api.herokuapp.com/api/profile/me";
+
+export const GET_USER_PROFILE = "GET_USER_PROFILE";
+
+export const getUserProfileAction = () => {
+  return async (dispatch, getState) => {
+    try {
+      let resp = await fetch(endpointUserProfile, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json;charset=UTF-8",
+          "Authentication": `Bearer ${process.env.REACT_APP_STRIVE_TOKEN}`,
+        }
+      })
+  
+      if (resp.ok) {
+        const data = await resp.json();
+  
+        dispatch({type: GET_USER_PROFILE, payload: data});
+      } else {
+        console.log("errore")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
