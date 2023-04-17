2c7e584130685d0816466f34cd275c3c283d79dc
phyxius23 <30ntin@gmail.com>
2023-04-17 15:48:01 +0200

    section profile

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -3,21 +3,25 @@ import { Button, Card, Carousel, Col, Container, Row } from "react-bootstrap";
 import coverImg from "../assets/img/cover.jpeg";
 import profileImg from "../assets/img/img-profile.jpg";
 import { Pencil } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 
 const MainComponent = () => {
   return (
     <Container>
       <Row>
         <Col md={9}>
-          <section className="rounded-3 border-dark profile">
+          <section className="rounded-3 border-dark mt-4 profile">
             <Row>
               <Col xs={12}>
                 <div className="hero-img">
                   <img src={coverImg} alt="" className="img-fluid" />
+                  <Link to="/">
+                    <Pencil />
+                  </Link>
                 </div>
               </Col>
               <Col xs={12}>
-                <div className="py-3 px-3">
+                <div className="p-3">
                   <div className="d-flex">
                     <div className="rounded-circle image-profile">
                       <img className="img-fluid" src={profileImg} alt="" />
@@ -48,37 +52,46 @@ const MainComponent = () => {
                 </div>
               </Col>
               <Col xs={12}>
-                {/* Carousel React Bootstrap */}
-                <Carousel slide={false}>
-                  <Carousel.Item>
-                    <Card>
-                      <Card.Body>
-                        <Card.Title className="truncate">Card Title</Card.Title>
-                        <Card.Text className="truncate">Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-                        <Card.Link>testo</Card.Link>
-                        {/* <Button variant="primary">Go somewhere</Button> */}
-                      </Card.Body>
-                    </Card>
-                  </Carousel.Item>
+                <div className="p-3">
+                  {/* Carousel React Bootstrap */}
+                  <Carousel slide={false}>
+                    <Carousel.Item>
+                      <Card>
+                        <Card.Body>
+                          <Card.Title className="truncate">Disponibile a lavorare 1</Card.Title>
+                          <Card.Text className="truncate">Ruoli di Web Designer, Frontend Web Developer, Full Stack Web Developer</Card.Text>
+                          <div>
+                            <Card.Link>Mostra Dettagli</Card.Link>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </Carousel.Item>
 
-                  <Carousel.Item>
-                    <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
+                    <Carousel.Item>
+                      <Card>
+                        <Card.Body>
+                          <Card.Title className="truncate">Disponibile a lavorare 2</Card.Title>
+                          <Card.Text className="truncate">Ruoli di Web Designer, Frontend Web Developer, Full Stack Web Developer</Card.Text>
+                          <div>
+                            <Card.Link>Mostra Dettagli</Card.Link>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </Carousel.Item>
 
-                    <Carousel.Caption>
-                      <h3>Second slide label</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-
-                  <Carousel.Item>
-                    <img className="d-block w-100" src="holder.js/800x400?text=Third slide&bg=20232a" alt="Third slide" />
-
-                    <Carousel.Caption>
-                      <h3>Third slide label</h3>
-                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                </Carousel>
+                    <Carousel.Item>
+                      <Card>
+                        <Card.Body>
+                          <Card.Title className="truncate">Disponibile a lavorare 3</Card.Title>
+                          <Card.Text className="truncate">Ruoli di Web Designer, Frontend Web Developer, Full Stack Web Developer</Card.Text>
+                          <div>
+                            <Card.Link>Mostra Dettagli</Card.Link>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </Carousel.Item>
+                  </Carousel>
+                </div>
               </Col>
             </Row>
           </section>
