f32dd5364912f13924a2a1c63e19a1823cfda6e0
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-04-18 15:56:28 +0200

    ExperiencePage dinamicizzata ed aggiunti button

--- src/component/ExperiencesPageComponents.jsx
+++ src/component/ExperiencesPageComponents.jsx
@@ -1,12 +1,15 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import EpicodeImg from "../assets/img/epicode-logo.png";
-import { ArrowLeft } from "react-bootstrap-icons";
+import { ArrowLeft, Pencil } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
 import SideComponent from "./SideComponent";
+import { useSelector } from "react-redux";
 
 const ExperiencesPageComponents = () => {
   const navigate = useNavigate();
-  const navigateToExperience = () => {
+  const experience = useSelector(state => state.experience.content);
+
+  const navigateToHome = () => {
     navigate("/");
   };
   return (
@@ -15,152 +18,40 @@ const ExperiencesPageComponents = () => {
         <Col md={9}>
           <section className="mt-4 rounded-3 border-dark profile p-3">
             <div className="d-flex">
-              <Button
-                variant="light"
-                onClick={navigateToExperience}
-                className="rounded-pill my-2"
-              >
-                <ArrowLeft className="fs-2" />
-              </Button>
-              <h3 className="ms-4 my-4">Esperienze</h3>
-            </div>
-            <div className="d-flex justify-content-start align-items-center">
-              <img
-                src={EpicodeImg}
-                alt=""
-                className="img-fluid mb-5"
-                width={50}
-              />
-              <div className="d-flex flex-column">
-                <p className="ms-3 fw-bold text-secondary">
-                  Front-end developer: HTML5, CSS3, SASS, UI/UX, ReactJS, Redux,
-                  TypeScript
-                </p>
-                <p className="ms-3 text-secondary">Strive School</p>
-                <p className="ms-3 text-secondary">
-                  giu 2019 - Presente - 3 anni 11 mesi
-                </p>
-                <p className="ms-3 mt-3">
-                  <span className="fw-bold">Competenze: </span> Prototyping ·
-                  Figma · Adobe XD · Agile · Scrum · Node.js · TypeScript · SASS
-                  · HTML5 · CSS3 · Redux · GitHub · Git
-                </p>
+              <div className="d-flex justify-content-start align-items-center">
+                <Button variant="light" onClick={navigateToHome} className="rounded-pill my-2">
+                  <ArrowLeft className="fs-2" />
+                </Button>
+                <h3 className="ms-3">Esperienze</h3>
               </div>
             </div>
-            <hr></hr>
-            <Row className="d-flex">
-              <Col md={9}>
-                <div className="d-flex justify-content-start align-items-center">
-                  <img
-                    src={EpicodeImg}
-                    alt=""
-                    className="img-fluid mb-5"
-                    width={50}
-                  />
-                  <div className="d-flex flex-column">
-                    <p className="ms-3 fw-bold text-secondary">
-                      Front-end developer: HTML5, CSS3, SASS, UI/UX, ReactJS,
-                      Redux, TypeScript
-                    </p>
-                    <p className="ms-3 text-secondary">Strive School</p>
-                    <p className="ms-3 text-secondary">
-                      giu 2019 - Presente - 3 anni 11 mesi
-                    </p>
-                    <p className="ms-3 mt-3">
-                      <span className="fw-bold">Competenze: </span> Prototyping
-                      · Figma · Adobe XD · Agile · Scrum · Node.js · TypeScript
-                      · SASS · HTML5 · CSS3 · Redux · GitHub · Git
-                    </p>
+            {experience &&
+              experience.map(item => (
+                <div key={item._id}>
+                  <div className="d-flex justify-content-start align-items-center">
+                    <img src={EpicodeImg} alt="" className="img-fluid mb-5" width={50} />
+                    <div className="d-flex justify-content-between  flex-grow-1">
+                      <div className="d-flex flex-column">
+                        <p className="ms-3 fw-bold text-secondary">{item.role}</p>
+                        <p className="ms-3 text-secondary">{item.company}</p>
+                        <p className="ms-3 text-secondary">
+                          {new Date(item.startDate).toLocaleDateString()} -{" "}
+                          {new Date(item.endDate).toLocaleDateString()}
+                        </p>
+                        <p className="ms-3 mt-3">
+                          <span className="fw-bold">Competenze: </span> {item.description}
+                        </p>
+                      </div>
+                      <div>
+                        <Button variant="light" className="rounded-pill mt-0 my-2 ml-auto">
+                          <Pencil />
+                        </Button>
+                      </div>
+                    </div>
                   </div>
+                  <hr></hr>
                 </div>
-              </Col>
-            </Row>
-            <hr></hr>
-            <Row className="d-flex">
-              <Col md={9}>
-                <div className="d-flex justify-content-start align-items-center">
-                  <img
-                    src={EpicodeImg}
-                    alt=""
-                    className="img-fluid mb-5"
-                    width={50}
-                  />
-                  <div className="d-flex flex-column">
-                    <p className="ms-3 fw-bold text-secondary">
-                      Front-end developer: HTML5, CSS3, SASS, UI/UX, ReactJS,
-                      Redux, TypeScript
-                    </p>
-                    <p className="ms-3 text-secondary">Strive School</p>
-                    <p className="ms-3 text-secondary">
-                      giu 2019 - Presente - 3 anni 11 mesi
-                    </p>
-                    <p className="ms-3 mt-3">
-                      <span className="fw-bold">Competenze: </span> Prototyping
-                      · Figma · Adobe XD · Agile · Scrum · Node.js · TypeScript
-                      · SASS · HTML5 · CSS3 · Redux · GitHub · Git
-                    </p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-            <hr></hr>
-
-            <Row className="d-flex">
-              <Col md={9}>
-                <div className="d-flex justify-content-start align-items-center">
-                  <img
-                    src={EpicodeImg}
-                    alt=""
-                    className="img-fluid mb-5"
-                    width={50}
-                  />
-                  <div className="d-flex flex-column">
-                    <p className="ms-3 fw-bold text-secondary">
-                      Front-end developer: HTML5, CSS3, SASS, UI/UX, ReactJS,
-                      Redux, TypeScript
-                    </p>
-                    <p className="ms-3 text-secondary">Strive School</p>
-                    <p className="ms-3 text-secondary">
-                      giu 2019 - Presente - 3 anni 11 mesi
-                    </p>
-                    <p className="ms-3 mt-3">
-                      <span className="fw-bold">Competenze: </span> Prototyping
-                      · Figma · Adobe XD · Agile · Scrum · Node.js · TypeScript
-                      · SASS · HTML5 · CSS3 · Redux · GitHub · Git
-                    </p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-            <hr></hr>
-
-            <Row className="d-flex">
-              <Col md={9}>
-                <div className="d-flex justify-content-start align-items-center">
-                  <img
-                    src={EpicodeImg}
-                    alt=""
-                    className="img-fluid mb-5"
-                    width={50}
-                  />
-                  <div className="d-flex flex-column">
-                    <p className="ms-3 fw-bold text-secondary">
-                      Front-end developer: HTML5, CSS3, SASS, UI/UX, ReactJS,
-                      Redux, TypeScript
-                    </p>
-                    <p className="ms-3 text-secondary">Strive School</p>
-                    <p className="ms-3 text-secondary">
-                      giu 2019 - Presente - 3 anni 11 mesi
-                    </p>
-                    <p className="ms-3 mt-3">
-                      <span className="fw-bold">Competenze: </span> Prototyping
-                      · Figma · Adobe XD · Agile · Scrum · Node.js · TypeScript
-                      · SASS · HTML5 · CSS3 · Redux · GitHub · Git
-                    </p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
+              ))}
           </section>
         </Col>
         <Col md={3}>
