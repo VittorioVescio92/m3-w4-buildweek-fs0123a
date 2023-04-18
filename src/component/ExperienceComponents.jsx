7a88f15836d27e965d5ba311ee367089706194c5
Nello Casolla <nellocasolla446@gmail.com>
2023-04-18 17:15:40 +0200

    modalexperience

--- src/component/ExperienceComponents.jsx
+++ src/component/ExperienceComponents.jsx
@@ -2,11 +2,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Button, Container } from "react-bootstrap";
 import EpicodeImg from "../assets/img/epicode-logo.png";
-import { ArrowRight } from "react-bootstrap-icons";
+import { ArrowRight, Pencil, PlusLg } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserExperienceAction } from "../redux/actions";
+import ModalProfile from "./ModalProfile";
+import ModalExperience from "./ModalExperience";
 
 const ExperienceComponents = () => {
   const dispatch = useDispatch();
@@ -22,23 +24,54 @@ const ExperienceComponents = () => {
   const navigateToExperience = () => {
     navigate("/experience");
   };
+
+  const [show, setShow] = useState(false);
+  const handleCloseModalEx = () => setShow(false);
+  const handleShowModalEx = () => setShow(true);
   return (
     <Container className="mt-2 d-flex p-0">
       <section className="rounded-3 border-dark profile p-3 w-100">
-        <h3>Esperienze</h3>
+        <div className="d-flex">
+          <h3 className="me-auto">Esperienze</h3>
+          <Button
+            onClick={handleShowModalEx}
+            variant="white"
+            className="d-flex text-secondary align-items-center fs-2"
+          >
+            <PlusLg />
+          </Button>
+          <ModalExperience
+            show={show}
+            handleCloseModalEx={handleCloseModalEx}
+          />
+          <Button
+            // onClick={navigateToModalExperience}
+            variant="white"
+            className="d-flex text-secondary align-items-center fs-3"
+          >
+            <Pencil />
+          </Button>
+        </div>
         {experience &&
           experience.map((item) => (
             <div key={item._id}>
               <div className="d-flex justify-content-start align-items-center">
-                <img src={EpicodeImg} alt="" className="img-fluid mb-5" width={50} />
+                <img
+                  src={EpicodeImg}
+                  alt=""
+                  className="img-fluid mb-5"
+                  width={50}
+                />
                 <div className="d-flex flex-column">
                   <p className="ms-3 fw-bold text-secondary">{item.role}</p>
                   <p className="ms-3 text-secondary">{item.company}</p>
                   <p className="ms-3 text-secondary">
-                    {new Date(item.startDate).toLocaleDateString()} - {new Date(item.endDate).toLocaleDateString()}
+                    {new Date(item.startDate).toLocaleDateString()} -{" "}
+                    {new Date(item.endDate).toLocaleDateString()}
                   </p>
                   <p className="ms-3 mt-3">
-                    <span className="fw-bold">Competenze: </span> {item.description}
+                    <span className="fw-bold">Competenze: </span>{" "}
+                    {item.description}
                   </p>
                 </div>
               </div>
@@ -46,7 +79,11 @@ const ExperienceComponents = () => {
             </div>
           ))}
         <div className="d-flex justify-content-center">
-          <Button onClick={navigateToExperience} variant="white" className="d-flex text-secondary align-items-center">
+          <Button
+            onClick={navigateToExperience}
+            variant="white"
+            className="d-flex text-secondary align-items-center"
+          >
             Mostra tutte le esperienze ({experience.length})
             <ArrowRight className="ms-2" />
           </Button>
