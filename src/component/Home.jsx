7b25a18d6ed84d0e06fe43539731b0f476f65ad8
Nello Casolla <nellocasolla446@gmail.com>
2023-04-19 15:42:52 +0200

    static home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,10 +2,15 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import avatar from "../avatar.png";
 import {
+  ArrowsFullscreen,
   CalendarDate,
   CardText,
+  ChatText,
+  HandThumbsUp,
   Image,
   PlayBtnFill,
+  SendFill,
+  ThreeDots,
 } from "react-bootstrap-icons";
 import { useState } from "react";
 import ModalHome from "./ModalHome";
@@ -29,7 +34,9 @@ const Home = () => {
   const handleShowHomeImg = () => setShowImg(true);
   return (
     <>
+      {/* CONTAINER GENERALE */}
       <div className="d-flex align-items-start mt-3 justify-content-center">
+        {/* COLONNA SINISTRA */}
         <div className="mx-3">
           <div className="mx-3 bg-white border rounded p-4">
             <div className="">
@@ -71,56 +78,61 @@ const Home = () => {
             <Button>Scopri di più</Button>
           </div>
         </div>
-        <div className="bg-white border rounded">
-          <div className="m-3">
-            <div className="d-flex mb-3">
-              <a>
-                <div className="m-2">
-                  <img
-                    id="avatar"
-                    src={avatar}
-                    alt="Avatar"
-                    className="rounded-circle"
-                    onClick={navigateHomePage}
-                  />
-                </div>
-              </a>
-              <Button
-                className="border"
-                variant="light"
-                onClick={handleShowHome}
-              >
-                {/* onclick modale */}
-                <span className="text-secondary">Avvia un post</span>
-              </Button>
-              <ModalHome show={show} handleCloseHome={handleCloseHome} />
-            </div>
-            <div>
-              <Button
-                variant="white"
-                className="p-0 mx-2"
-                onClick={handleShowHomeImg}
-              >
-                <Image className="text-primary" />
-                <span className="mx-2 text-secondary">Foto</span>
-              </Button>
-              <ModalHomeImg
-                showImg={showImg}
-                handleCloseHomeImg={handleCloseHomeImg}
-              />
+        <div>
+          {/* COLONNA CENTRALE */}
+          <div className="bg-white border rounded">
+            <div className="m-3">
+              <div className="d-flex mb-3">
+                <a>
+                  <div className="m-2">
+                    <img
+                      id="avatar"
+                      src={avatar}
+                      alt="Avatar"
+                      className="rounded-circle"
+                      onClick={navigateHomePage}
+                    />
+                  </div>
+                </a>
+                <Button
+                  className="border"
+                  variant="light"
+                  onClick={handleShowHome}
+                >
+                  {/* onclick modale */}
+                  <span className="text-secondary">Avvia un post</span>
+                </Button>
+                <ModalHome show={show} handleCloseHome={handleCloseHome} />
+              </div>
+              <div>
+                <Button
+                  variant="white"
+                  className="p-0 mx-2"
+                  onClick={handleShowHomeImg}
+                >
+                  <Image className="text-primary" />
+                  <span className="mx-2 text-secondary">Foto</span>
+                </Button>
+                <ModalHomeImg
+                  showImg={showImg}
+                  handleCloseHomeImg={handleCloseHomeImg}
+                />
 
-              <Button variant="white" className="p-0 mx-2">
-                <PlayBtnFill className="text-success" />
-                <span className="mx-2 text-secondary">Video</span>
-              </Button>
-              <Button variant="white" className="p-0 mx-2">
-                <CalendarDate className="text-warning" />
-                <span className="mx-2 text-secondary">Evento</span>
-              </Button>
-              <Button variant="white" className="p-0 mx-2">
-                <CardText className="text-danger" />
-                <span className="mx-2 text-secondary">Scrivi un articolo</span>
-              </Button>
+                <Button variant="white" className="p-0 mx-2">
+                  <PlayBtnFill className="text-success" />
+                  <span className="mx-2 text-secondary">Video</span>
+                </Button>
+                <Button variant="white" className="p-0 mx-2">
+                  <CalendarDate className="text-warning" />
+                  <span className="mx-2 text-secondary">Evento</span>
+                </Button>
+                <Button variant="white" className="p-0 mx-2">
+                  <CardText className="text-danger" />
+                  <span className="mx-2 text-secondary">
+                    Scrivi un articolo
+                  </span>
+                </Button>
+              </div>
             </div>
           </div>
           {/* <div className="d-flex">
@@ -129,7 +141,68 @@ const Home = () => {
           ordina
         </span>
       </div> */}
+          {/* <button variant="success" className="d-flex mx-1">
+            <hr className="d-flex flex-grow-1 mr-2"></hr>
+            <div className="d-flex">
+              <span className="d-flex">Ordina per:</span>
+              <span className="d-flex">Principali</span>
+            </div>
+          </button> */}
+          <div className="bg-white border rounded mt-3">
+            <div className="m-3">
+              <div className="d-flex mb-3">
+                <img src={avatar} width={80} />
+                <div>
+                  <a>Acquatech</a>
+                  <p>8.118 follower</p>
+                  <p>Post sponsorizzato</p>
+                </div>
+                <Button className="ms-auto" variant="white">
+                  <ThreeDots />
+                </Button>
+              </div>
+              <p>
+                Explore water business opportunity for the pizza in the pasta
+              </p>
+              <div>
+                <img src={avatar} width={400} />
+                <div>
+                  <a>acquatechrade.com</a>
+                  <br />
+                  <span className="me-5">Acquatech China: 5-7 June 2023</span>
+                  <Button className="ms-5">Scopri di più</Button>
+                </div>
+              </div>
+              <div>
+                36
+                <HandThumbsUp className="me-5" />
+                <span className="ms-5">1 diffusione post</span>
+              </div>
+              <hr></hr>
+              <div>
+                <Button variant="white">
+                  <HandThumbsUp />
+                  <span className="mx-2">Consiglia</span>
+                </Button>
+                <Button variant="white">
+                  <ChatText />
+                  <span className="mx-2">Commenta</span>
+                </Button>
+                <Button variant="white">
+                  <ArrowsFullscreen />
+                  <span className="mx-2">Diffondi il post</span>
+                </Button>
+                <Button variant="white">
+                  <SendFill />
+                  <span className="mx-2">Invia</span>
+                </Button>
+              </div>
+              <p>ℹ️ Scrivi il tuo primo commento</p>
+            </div>
+          </div>
         </div>
+
+        {/* COLONNA DESTRA */}
         <div className="mx-3">
           <div className="mx-3 bg-white border rounded p-4">
             <div>
